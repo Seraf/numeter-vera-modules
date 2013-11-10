ec1233dc2226fe6856747e11b826176c06b1401a
Seraf <julien.syx@gmail.com>
2013-11-10 20:07:18 +0100

    Changing the version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from distutils.core import setup
 if __name__ == '__main__':
 
     setup(name='numeter-vera-module',
-          version='0.0.1',
+          version='0.0.2',
           description='Numeter Veras Poller module',
           long_description="""A Vera module for Numeter poller.""",
           author='Julien Syx',
