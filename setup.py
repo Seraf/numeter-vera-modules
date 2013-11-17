3a67d7d1877e4672eed9b418afbfff9c19814a5f
root <root@Tagada-server.bouyguesbox.fr>
2013-11-17 11:01:15 +0100

    Changing the setup file

--- setup.py
+++ setup.py
@@ -1,47 +1,52 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# $Id: setup.py,v 0.2.3.4 2013-02-27 09:56:56 gaelL Exp $
+# $Id: setup.py,v 0.2.3.4 2013-11-15 09:56:56 Seraf Exp $
 #
-# Copyright (C) 2010-2013  Gaël Lambert (gaelL) <gael@gael-lambert.org>
+# Copyright (C) 2013  Julien Syx (Seraf) <julien@syx.fr>
 #
-# Numeter is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-# 
-# You should have received a copy of the GNU Affero General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
-
+#The MIT License (MIT)
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy of
+#this software and associated documentation files (the "Software"), to deal in
+#the Software without restriction, including without limitation the rights to
+#use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+#the Software, and to permit persons to whom the Software is furnished to do so,
+#subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+#FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+#COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+#IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+#CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 from distutils.core import setup
 
 
 if __name__ == '__main__':
 
-    setup(name='numeter-vera-module',
+    setup(name='numeter-vera-modules',
           version='0.0.5',
-          description='Numeter Veras Poller module',
+          description='Numeter Vera Poller modules',
           long_description="""A Vera module for Numeter poller.""",
           author='Julien Syx',
           author_email='julien@syx.fr',
           maintainer='Julien Syx',
           maintainer_email='julien@syx.fr',
           keywords=['numeter','graphing','poller','vera','homeautomation'],
-          url='https://github.com/Seraf/numeter-vera-module',
+          url='https://github.com/Seraf/numeter-vera-modules',
           license='MIT',
           packages = [''],
           package_data={'': ['veraModule.py']},
           classifiers=[
               'Development Status :: 4 - Beta',
               'Environment :: Console',
-              'Intended Audience :: Advanced End Users',
+              'Intended Audience :: End Users/Desktop',
               'Intended Audience :: System Administrators',
-              'License :: OSI Approved :: MIT',
+	      'License :: OSI Approved :: MIT License',
               'Operating System :: POSIX',
               'Programming Language :: Python',
               'Topic :: System :: Monitoring'
